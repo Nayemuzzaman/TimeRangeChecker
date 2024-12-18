d7dbb0f64b82a47f075ddd602c10a2d3c1251cdc
nayemuzzaman <nayemzaman@gmail.com>
2024-12-18 23:22:20 +0900

    Initial Commit

---
+++ TimeRangeChecker/TimeRangeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRangeCheckerApp.swift
+//  TimeRangeChecker
+//
+//  Created by Nayemuzzaman on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeRangeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
